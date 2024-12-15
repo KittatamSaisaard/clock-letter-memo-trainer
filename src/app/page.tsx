58eb6d1a7b890a9d4135021ebe8ad55198324474
Kit <50354903+ksaisaard@users.noreply.github.com>
2024-12-16 09:12:21 +1030

    Added instructions.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-center text-6xl p-3">Clock Letter Memo Trainer</h1>
+    <div className="flex flex-col items-center">
+      <h1 className="text-center text-6xl p-3 pb-12">Clock Letter Memo Trainer</h1>
+      <div className="p-3">
+        <h3 className="text-center text-3xl">Instructions:</h3>
+        <ul className="list-disc list-inside text-left">
+          <li>
+          A letter will appear on the screen.
+          </li>
+          <li>
+          Select the clock hour corresponding to the letter by clicking the correct button (A-F = 1-6, G-K = 7-11, O = 0).
+          </li>
+          <li>
+          Review the results after each round to see any mistakes and the correct answers.
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
