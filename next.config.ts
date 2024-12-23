3204cdcd9a3a6b9dcbaed75eda145441beccd191
Kit <50354903+ksaisaard@users.noreply.github.com>
2024-12-23 18:20:33 +1030

    Added basePath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/clock-letter-memo-trainer",
+  // other config options here
 };
 
 export default nextConfig;
