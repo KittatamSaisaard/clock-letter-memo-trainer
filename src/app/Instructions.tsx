365124e082a2561288d0dba3548f52e649770a2c
Kit <50354903+ksaisaard@users.noreply.github.com>
2024-12-23 17:40:43 +1030

    Removed review results after each round instruction

--- src/app/Instructions.tsx
+++ src/app/Instructions.tsx
@@ -7,7 +7,6 @@ export default function Instructions() {
         <ul className="list-disc list-inside text-left">
           <li>A letter will appear on the screen.</li>
           <li>Select the clock hour corresponding to the letter by clicking the correct button (A-F = 1-6, G-K = 7-11, O = 0).</li>
-          <li>Review the results after each round to see any mistakes and the correct answers.</li>
         </ul>
       </div>
   );
