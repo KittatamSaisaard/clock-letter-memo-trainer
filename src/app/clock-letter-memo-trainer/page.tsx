6822ce20645f223b06b91d6a4493a99e6ad90503
Kit <50354903+ksaisaard@users.noreply.github.com>
2024-12-29 16:30:13 +1030

    Added a back button to the main menu

--- src/app/clock-letter-memo-trainer/page.tsx
+++ src/app/clock-letter-memo-trainer/page.tsx
@@ -1,10 +1,20 @@
 import Instructions from './Instructions';
 import ButtonWrapper from './ButtonWrapper';
+import Link from 'next/link';
 
 
 export default function ClockLetterMemoTrainer() {
+  const navigateToHome = () => {
+    window.location.href = '/';
+  };
+
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center relative">
+      {/* Back to Home Button */}
+      <Link href="/" className="absolute top-5 left-5 px-4 py-2 bg-blue-500 text-white rounded">
+        Main Menu
+      </Link>
+
       {/* Title */}
       <h1 className="text-center text-6xl p-3 pb-12">Clock Letter Memo Trainer</h1>
 
