6389b649064c0ee0fc4a2aad758c8795026a7f51
Kit <50354903+ksaisaard@users.noreply.github.com>
2025-01-25 23:38:32 +1030

    Added clock simulator page

---
+++ src/app/clock-simulator/page copy.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function ClockLetterMemoTrainer() {
+
+  return (
+    <div className="flex flex-col items-center relative min-h-screen bg-gray-100">
+      {/* Title */}
+      <h1 className="text-center text-6xl p-3 pb-12">Clock Simulator</h1>
+
+      {/* Back to Home Button */}
+      <Link href="/" className="mb-6 px-4 py-2 bg-blue-500 text-white rounded">
+        Explore Other Tools!
+      </Link>
+    </div>
+  );
+}
