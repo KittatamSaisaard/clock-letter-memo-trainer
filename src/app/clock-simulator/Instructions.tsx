513abe2383f8e47f692b54373c2155ceefbf0222
Kit <50354903+KittatamSaisaard@users.noreply.github.com>
2025-01-28 17:50:10 +1030

    Added instructions component to clock simulator component

---
+++ src/app/clock-simulator/Instructions.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Instructions() {
+  return (
+      <div className="p-3">
+        <h3 className="text-center text-3xl">Instructions:</h3>
+      </div>
+  );
+}
