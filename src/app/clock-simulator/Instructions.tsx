b2dcafdbd55f7b2530b961c49766f58e0339d203
Kit <50354903+KittatamSaisaard@users.noreply.github.com>
2025-02-04 11:49:13 +1100

    Added description for clock simulator

--- src/app/clock-simulator/Instructions.tsx
+++ src/app/clock-simulator/Instructions.tsx
@@ -3,7 +3,9 @@
 export default function Instructions() {
   return (
       <div className="p-3">
+        <h2 className="text-center">The Rubik's Clock Simulator allows you to interact with a virtual Rubik's Clock</h2>
         <h3 className="text-center text-3xl">Instructions:</h3>
+        <p></p>
       </div>
   );
 }
